8df32249bc52ba29174ad4854bd516d1df2929a5
seanox <service@seanox.de>
2016-08-09 07:44:11 +0200

    #0000

--- sources/com/seanox/devwex/Remote.java
+++ sources/com/seanox/devwex/Remote.java
@@ -3,7 +3,7 @@
  *  im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
  *  Diese Software unterliegt der Version 2 der GNU General Public License.
  *
- *  Devwex, Advanced Server Developing
+ *  Devwex, Advanced Server Development
  *  Copyright (C) 2016 Seanox Software Solutions
  *
  *  This program is free software; you can redistribute it and/or modify it

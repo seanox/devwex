f4478bea0206ae48a395ad617a23111dfa2c5baa
seanox <service@seanox.de>
2017-10-03 21:11:15 +0200

    #0000

--- manuals/seanox-devwex.js
+++ manuals/seanox-devwex.js
@@ -12,7 +12,24 @@
         while (text.length < (size || 2))
             text = "0" + text;
         return text;
-   };
+    };
+})();
+
+(function() {
+    if (typeof Element.prototype.show === "function")
+        return false;
+    Element.prototype.show = function() {
+        this.className = (" " + this.className + " ").replace(/\s+hidden\s+/ig, ' ').trim();        
+    };
+})();
+
+(function() {
+    if (typeof Element.prototype.hide === "function")
+        return false;
+    Element.prototype.hide = function() {
+        this.show();
+        this.className = (this.className + " hidden").trim();
+    };
 })();
 
 window.addEventListener("load", function() {
@@ -31,8 +48,24 @@ window.addEventListener("load", function() {
 });
 
 window.addEventListener("load", function() {
-    var toc = document.querySelector("body > section > article:nth-child(3)");
-    var elements = document.querySelectorAll("body > section > article");
+    var elements = document.querySelector("table.architecture");
+    elements = elements.getElementsByTagName("*");
+    elements = Array.prototype.slice.call(elements, 0);
+    elements.forEach(function(element, index, array) {
+        element.className = (element.className + " architecture-" + (index).pad(4)).trim();
+    });
+});
+
+window.addEventListener("load", function() {
+    var elements = document.querySelectorAll("body > main > article:nth-child(n+2):not(:nth-child(3))");
+    elements.forEach(function(element, index, array) {
+        element.hide();
+    });
+});
+
+window.addEventListener("load", function() {
+    var toc = document.querySelector("body > main > article > nav");
+    var elements = document.querySelectorAll("body > main > article");
     var numbers = [0, 0, 0, 0, 0, 0, 0];
     var sitemap = new Object();
     var lookup = function(number) {
@@ -59,7 +92,7 @@ window.addEventListener("load", function() {
     elements.forEach(function(element, index, array) {
         if (index < 2)
             return;
-        var elements = element.querySelectorAll("h1, h2, h3, h4, h5, h6");
+        var elements = element.querySelectorAll("article:not(:nth-child(3)) h1, article h2, article h3, article h4, article h5, article h6");
         elements.forEach(function(element, index, array) {
             var level = parseInt(element.nodeName.match(/\d+$/));
             if (level >= 1
@@ -73,10 +106,12 @@ window.addEventListener("load", function() {
                 text = text.replace(/"/g, "&quot;");
                 sitemap[numbers.slice(1).join(".")] = text;
                 sitemap[lookup(numbers.slice(1).join("."))] = text;
+                element.setAttribute("chapter", numbers.slice(1).join(".").replace(/(\.0)+$/g, ""));
             }
             var number = numbers.slice(1).join(".");
+            var chapter = numbers.slice(1).join(".").replace(/(\.0)+$/g, "");
             var text = lookup(number);
-            toc.innerHTML += "<toc" + level + ">"
+            toc.innerHTML += "<toc" + level + " chapter=\"" + chapter + "\">"
                 + "<a href=\"#" + text + "\">" + element.textContent + "</a>"
                 + "</toc" + level + ">";
             element.innerHTML = "<a name=\"" + number + "\"></a>"
@@ -84,9 +119,9 @@ window.addEventListener("load", function() {
                               + element.innerHTML;
         });
     });
-    
+
     var pattern = new RegExp("\\[#([a-z0-9]+)\\]", "ig");
-    elements = document.querySelectorAll("body > section > article");
+    elements = document.querySelectorAll("body > main > article");
     elements.forEach(function(element, index, array) {
         if (!pattern.test(element.innerHTML))
             return;
@@ -94,11 +129,47 @@ window.addEventListener("load", function() {
             return "<a href=\"#" + word + "\">" + sitemap[word] + "</a>";
         });
     });
-    
-    elements = document.querySelector("table.architecture");
-    elements = elements.getElementsByTagName("*");
-    elements = Array.prototype.slice.call(elements, 0);
+
+    elements = document.querySelectorAll("toc1 a, toc2 a, toc3 a, toc4 a, toc5 a, toc6 a");
     elements.forEach(function(element, index, array) {
-        element.className = (element.className + " architecture-" + (index).pad(4)).trim();
+        element.parentNode.replaceChild(element.cloneNode(true), element);
+    });
+    elements = document.querySelectorAll("toc1 a, toc2 a, toc3 a, toc4 a, toc5 a, toc6 a");
+    elements.forEach(function(element, index, array) {
+        element.addEventListener("click", function() {
+            var elements = document.querySelectorAll("body > main > article:not(:nth-child(3))");
+            elements.forEach(function(element, index, array) {
+                element.hide();
+            });
+            var chapter = parseInt(this.parentNode.getAttribute("chapter").match(/^\d+/));
+            var element = document.querySelector("body > main > article:nth-child(" + (chapter +3) + ")");
+            element.show();
+        });
     });
 });
+
+window.addEventListener("load", function() {
+    //TODO: add event listener for nav-buttons
+});
+
+var Toc = Toc || {};
+
+Toc.show = function(chapter) {
+}
+
+var Manual = Manual || {};
+
+Manual.onToc = function(chapter) {
+};
+
+Manual.onChapterStart = function() {
+};
+
+Manual.onChapterEnd = function() {
+};
+
+Manual.onChapterUp = function() {
+};
+
+Manual.onChapterDown = function() {
+};

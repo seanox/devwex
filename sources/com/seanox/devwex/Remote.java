f80fc2c6ae4e530c7b878128aff913ed1478e44e
seanox <service@seanox.de>
2023-12-10 09:27:15 +0100

    #0000 Project: Update of slogan

--- sources/com/seanox/devwex/Remote.java
+++ sources/com/seanox/devwex/Remote.java
@@ -3,7 +3,7 @@
  * Folgenden Seanox Software Solutions oder kurz Seanox genannt.
  * Diese Software unterliegt der Version 2 der Apache License.
  *
- * Devwex, Advanced Server Development
+ * Devwex, Minimalistic Server Engine
  * Copyright (C) 2023 Seanox Software Solutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not

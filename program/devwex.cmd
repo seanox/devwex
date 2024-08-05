d0e91c7b68fc87a956a841b012f2b936195c102f
seanox <service@seanox.de>
2024-08-05 22:00:56 +0200

    #0000 CR: Windows: Optimization of the runtime directory

--- program/devwex.cmd
+++ program/devwex.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-rem Seanox Devwex is started from working directory ./devwex/program.
+rem Seanox Devwex is started from working directory .\devwex\program.
 rem Here, following variables are used:
 rem
 rem   CLASSPATH     path of java resources
@@ -22,11 +22,10 @@ rem
 rem   SYSTEMROOT    standard variable SYSTEMROOT of Windows, this is used of
 rem                 some CGI application to find systems components
 rem
-rem With startup all batch scripts are loaded from path ../runtime/scripts.
+rem With startup all batch scripts are loaded from path ..\runtime.
 rem Please note, that scripts to extend of runtime environment are relative to
 rem working directory.
 
-color 8F
 cls
 
 set CLASSPATH=
@@ -40,27 +39,43 @@ rem - in the work directory ..\runtime\java
 rem - else if JAVA_HOME is set
 rem - else Java runtime in the PATH variable
 
-if "%JAVAPATH%" == "" (
-  if exist "%cd%\..\runtime\java\bin\java.exe" set JAVAPATH=%cd%\..\runtime\java\bin
-)
+SET RUNTIME=..\runtime
+
+SetLocal EnableDelayedExpansion
+
 if "%JAVAPATH%" == "" (
   if not "%JAVA_HOME%" == "" (
-    if exist "%JAVA_HOME%\bin\java.exe" set JAVAPATH=%JAVA_HOME%\bin
+    if exist "%JAVA_HOME%\bin\java.exe"^
+        set JAVAPATH=%JAVA_HOME%\bin
   )
 )
 if "%JAVAPATH%" == "" (
   for %%i in ("%PATH:;=";"%") do (
-    if exist "%%i\java.exe" set JAVAPATH=%%i
+    if exist "%%i\java.exe"^
+        set JAVAPATH=%%i
   )
 )
 
-for %%f in (../runtime/scripts/*.bat ../runtime/scripts/*.cmd) do call ../runtime/scripts/%%f
+for /f "delims=: " %%d in ('dir /AD /B %RUNTIME%') do (
+  set DIRECTORY=%cd%\%RUNTIME%\%%d
+  set DIRECTORY=!DIRECTORY:\\=\!
+  set PATH=!DIRECTORY!;!PATH!
+  if exist "!DIRECTORY!\bin"^
+      set PATH=!DIRECTORY!\bin;!PATH!
+  if "!JAVAPATH!" == "" (
+  if exist "!DIRECTORY!\bin\java.exe"^
+     if exist "!DIRECTORY!\bin\java.exe"^
+         set JAVAPATH=!DIRECTORY!\bin
+  )
+)
+
+for %%f in (%RUNTIME%\*.bat %RUNTIME%\*.cmd) do call %RUNTIME%\%%f
 
 if not exist "%JAVAPATH%\java.exe" (
   echo Seanox Devwex Service 0.0.0 00000000
   echo Copyright ^(C^) 0000 Seanox Software Solutions
   echo Experimental Server Engine
-  echo. 
+  echo.
   echo ERROR: Java Runtime Environment not found
   goto :EOF
 )
@@ -70,4 +85,4 @@ set OPTIONS=%OPTIONS% -Dsystemdrive=%SYSTEMDRIVE%
 set OPTIONS=%OPTIONS% -Dsystemroot="%SYSTEMROOT%"
 set OPTIONS=%OPTIONS% -Dlibraries="..\libraries;%LIBRARIESPATH%;"
 
-"%JAVAPATH\%java.exe" -cp "devwex.jar;%CLASSPATH%" %OPTIONS% com.seanox.devwex.Service %1
+"%JAVAPATH%\java.exe" -cp "devwex.jar;%CLASSPATH%" %OPTIONS% com.seanox.devwex.Service %1

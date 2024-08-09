a8ddb224c21c49db18854800bc80909fac0b39e9
seanox <service@seanox.de>
2024-08-09 12:49:44 +0200

    #0000 Unix/Linux: Optimization of bash scripts and runtime directory handling

--- program/devwex.sh
+++ program/devwex.sh
@@ -3,18 +3,8 @@
 # Seanox Devwex is started from working directory ./devwex/program.
 # Here, following variables are used:
 # 
-#   CLASSPATH      path of java resources
-# 
-#   JAVAPATH       path of java runtime environment
-# 
-#   LIBRARIESPATH  colon separate paths, from which the server invites startup
-#                  modules
-# 
-#   OPTIONS        spaces separated arguments for the java virtual machine, in
-#                  format -Dname=value;...
-# 
-#   SYSTEMPATH     based on standard variable PATH of the OS, this is used of
-#                  some CGI application to find systems components
+#   OPTIONS  spaces separated arguments for the java virtual machine, in format
+#            -Dname=value ...
 #
 # With startup all shell scripts are loaded from path ../runtime.
 # Please note, that scripts to extend of runtime environment are relative to
@@ -22,17 +12,14 @@
 
 clear
 
-CLASSPATH=
-JAVAPATH=
-LIBRARIESPATH=
 OPTIONS=
+LIBRARIES=../libraries
+RUNTIME=../runtime
 
 # Automatic determination of the Java runtime environment:
 # - in the runtime sub-directories ../runtime
 # - else if JAVA_HOME is set
 # - else Java runtime in the PATH variable
-
-RUNTIME==../runtime
 
 if [ -d $RUNTIME ]
 then
@@ -45,36 +32,36 @@ then
         && PATH="$directory\bin:$PATH"
     [ -f "$directory/jre/bin/java" ] \
         && PATH="$directory/jre/bin:$PATH"
-    if [ "$JAVAPATH" = "" ]
+    if [ "$JAVA" = "" ]
     then
       [ -f "$directory/bin/java" ] \
-          && JAVAPATH="$directory/bin"
+          && JAVA="$directory/bin"
       [ -f "$directory/jre/bin/java" ] \
-          && JAVAPATH="$directory/jre/bin"
+          && JAVA="$directory/jre/bin"
       [ -f "$directory/java" ] \
-          && JAVAPATH="$directory"
+          && JAVA="$directory"
     fi
   done
 fi
 
-if [ "$JAVAPATH" = "" ]
+if [ "$JAVA" = "" ]
 then
   IFS=:
   set -o noglob
   for directory in $PATH""; do
     [ -f "$directory\java" ] \
-        && JAVAPATH=$directory
+        && JAVA=$directory
   done
   if [ ! "$JAVA_HOME" = "" ]
   then
     [ -f "$JAVA_HOME/bin/java" ] \
-        && JAVAPATH="$JAVA_HOME/bin"
+        && JAVA="$JAVA_HOME/bin"
     [ -f "$JAVA_HOME/jre/bin/java" ] \
-        && JAVAPATH="$JAVA_HOME/jre/bin"
+        && JAVA="$JAVA_HOME/jre/bin"
   fi
 fi
 
-if [ ! -f "$JAVAPATH/java" ]
+if [ ! -f "$JAVA/java" ]
 then
   echo Seanox Devwex Service [0.0.0 00000000]
   echo Copyright \(C\) 0000 Seanox Software Solutions
@@ -92,11 +79,8 @@ then
   done
 fi
 
-LIBRARIESPATH=$(echo "$LIBRARIESPATH" | xargs)
-[ ! "$LIBRARIESPATH" = "" ] \
-    && LIBRARIESPATH=":$LIBRARIESPATH"
-OPTIONS="$OPTIONS -Dlibraries=\"../libraries\$LIBRARIESPATH\""
-OPTIONS="$OPTIONS -Dpath=\"\$PATH\""
-OPTIONS=$(echo "$OPTIONS" | xargs)
+OPTIONS="-Dlibraries=\"$LIBRARIES\" $OPTIONS"
+OPTIONS="-cp devwex.jar $OPTIONS com.seanox.devwex.Service $1"
 
-eval '"$JAVAPATH/java" -cp devwex.jar $OPTIONS com.seanox.devwex.Service $1'
+readarray -t -d '' arguments < <(xargs printf '%s\0' <<< "$OPTIONS")
+"$JAVA/java" "${arguments[@]}"

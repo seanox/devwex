9e6b88f072635707bd92a456a2c70b43d3d7b36b
seanox <service@seanox.com>
2024-10-14 20:26:54 +0200

    #0000 HTTP(S): Correction of the Accept header when using CGI

--- sources/com/seanox/devwex/Worker.java
+++ sources/com/seanox/devwex/Worker.java
@@ -1524,7 +1524,7 @@ class Worker implements Runnable {
                     offset = this.mediatype.indexOf("/");
                     if (offset >= 0
                             && (entry.equals(this.mediatype.substring(0, offset + 1).concat("*"))
-                            || entry.equals(("*").concat(this.mediatype.substring(offset)))))
+                                    || entry.equals(("*").concat(this.mediatype.substring(offset)))))
                         break;
                 } else this.status = 406;
             }

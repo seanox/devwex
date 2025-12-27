fb18ee021a5c652734c2468b4898f0d168d980aa
seanox <service@seanox.com>
2025-12-27 08:20:19 +0100

    #0000 Review: Optimization and corrections

--- test/resources/libraries/extras/Parameter.java
+++ test/resources/libraries/extras/Parameter.java
@@ -1,6 +1,6 @@
 /**
  * Devwex, Experimental Server Engine
- * Copyright (C) 2022 Seanox Software Solutions
+ * Copyright (C) 2025 Seanox Software Solutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of

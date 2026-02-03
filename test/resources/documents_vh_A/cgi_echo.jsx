b45a1de0b56ae731b523a0a935ec8b888aa2639c
seanox <service@seanox.com>
2026-02-03 14:57:22 +0100

    #0000 Test: Preparation for LTS tests

--- test/resources/documents_vh_A/cgi_echo.jsx
+++ test/resources/documents_vh_A/cgi_echo.jsx
@@ -1,10 +1,9 @@
-var System = Java.type("java.lang.System");
+var System = java.lang.System;
 
 System.out.print("HTTP/1.0 200\r\n\r\n");
-var byteArray = Java.type("byte[]");
-var bytes = new byteArray(65535);
+var bytes = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 65535);
 var size;
-while ((size = System.in.read(bytes)) >= 0) {
+while ((size = System['in'].read(bytes)) >= 0) {
     System.out.write(bytes, 0, size);
     System.out.flush();
 }

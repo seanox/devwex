79a4347409cb95c3e4a3e5bee0bd6707e2553a9c
seanox <service@seanox.com>
2026-02-03 19:35:05 +0100

    #0000 Test: Preparation for LTS tests

--- test/resources/documents_vh_A/cgi_count.jsx
+++ test/resources/documents_vh_A/cgi_count.jsx
@@ -1,16 +1,16 @@
-var File = Java.type("java.io.File");
-var OutputStream = Java.type("java.io.OutputStream");
-var FileOutputStream = Java.type("java.io.FileOutputStream");
-var PrintWriter = Java.type("java.io.PrintWriter");
-var Thread = Java.type("java.lang.Thread");
-var System = Java.type("java.lang.System");
-var Integer = Java.type("java.lang.Integer");
+var File = java.io.File;
+var FileOutputStream = java.io.FileOutputStream;
+var PrintWriter = java.io.PrintWriter;
+var Thread = java.lang.Thread;
+var System = java.lang.System;
+var Integer = java.lang.Integer;
 
-var file = new File(System.getenv("DOCUMENT_ROOT") + "/cgi_count.txt");
-file.delete();
+var docroot = System.getenv("DOCUMENT_ROOT");
+var file = new File(docroot + "/cgi_count.txt");
+file["delete"]();
 for (var count = 1; count < 1000; count++) {
-    output = new FileOutputStream(file);
-    writer = new PrintWriter(output);
+    var output = new FileOutputStream(file);
+    var writer = new PrintWriter(output);
     writer.print(Integer.valueOf(count));
     writer.flush();
     output.close();

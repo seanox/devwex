39133dc029d69abef95451bd318bb5f45d979804
seanox <service@seanox.com>
2026-02-03 14:42:51 +0100

    #0000 Test: Preparation for LTS tests

--- test/resources/documents/commons/lastmodified.jsx
+++ test/resources/documents/commons/lastmodified.jsx
@@ -1,12 +1,12 @@
-var System = Java.type("java.lang.System");
-var File = Java.type("java.io.File");
-var SimpleDateFormat = Java.type("java.text.SimpleDateFormat");
-var Locale = Java.type("java.util.Locale");
+var System = java.lang.System;
+var File = java.io.File;
+var SimpleDateFormat = java.text.SimpleDateFormat;
+var Locale = java.util.Locale;
 
 System.out.print("HTTP/1.0 200\r\n\r\n");
-var file = System.getenv().get("HTTP_FILE") || "";
+var file = new java.lang.String(System.getenv().get("HTTP_FILE") || "");
 file = file.matches("^[^\\\\/].*$") ? new File(new File("../../.."), file) : new File(file);
-file = file.getCanonicalFile()
+file = file.getCanonicalFile();
 if (file.exists()) {
     var format = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z", Locale.US);
     System.out.print(format.format(file.lastModified()));
